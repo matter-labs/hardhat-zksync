a10f461e7b32fe9a6c4e77169957bc4b078dc3cd
Igor Aleksanov <popzxc@yandex.ru>
2021-11-23 12:09:30 +0300

    Add a bit of documentation for deployer

--- packages/hardhat-zksync-deploy/src/deployer.ts
+++ packages/hardhat-zksync-deploy/src/deployer.ts
@@ -3,6 +3,9 @@ import { Artifact, HardhatRuntimeEnvironment } from "hardhat/types";
 
 const ARTIFACT_FORMAT_VERSION = "hh-zksolc-artifact-1";
 
+/**
+ * An entity capable of deploying contracts to the zkSync network.
+ */
 export class Deployer {
     public hre: HardhatRuntimeEnvironment;
 
@@ -10,14 +13,41 @@ export class Deployer {
         this.hre = hre;
     }
 
-    public async loadArtifact(artifactId: string): Promise<Artifact> {
-        const artifact = await this.hre.artifacts.readArtifact(artifactId);
+  /**
+   * Loads an artifact and verifies that it was compiled by `zksolc\.
+   *
+   * @param contractNameOrFullyQualifiedName The name of the contract.
+   *   It can be a contract bare contract name (e.g. "Token") if it's
+   *   unique in your project, or a fully qualified contract name
+   *   (e.g. "contract/token.sol:Token") otherwise.
+   *
+   * @throws Throws an error if a non-unique contract name is used,
+   *   indicating which fully qualified names can be used instead.
+   *
+   * @throws Throws an error if an artifact was not compiled by `zksolc`.
+   */
+    public async loadArtifact(contractNameOrFullyQualifiedName: string): Promise<Artifact> {
+        const artifact = await this.hre.artifacts.readArtifact(contractNameOrFullyQualifiedName);
         
         // Verify that this artifact was compiled by the zkSync compiler, and not `solc` or `vyper`.
         if (artifact._format != ARTIFACT_FORMAT_VERSION) {
-            throw pluginError(`Artifact ${artifactId} was not compiled by zksolc`);
+            throw pluginError(`Artifact ${contractNameOrFullyQualifiedName} was not compiled by zksolc`);
         }
         return artifact;
+    }
+
+    /**
+     * Sends a deploy transaction to the zkSync network.
+     * 
+     * @param artifact The previously loaded artifact object.
+     * 
+     * @returns A handle for the deploy transaction.
+    */
+    public async deploy(artifact: Artifact): Promise<any> {
+        // TODO:
+        // - Form the transaction.
+        // - Send it via Web3.
+        // - Return a transaction handle.
     }
 }
 
--- packages/hardhat-zksync-deploy/test/tests.ts
+++ packages/hardhat-zksync-deploy/test/tests.ts
@@ -2,14 +2,19 @@ import { useEnvironment } from "./helpers";
 import { assert } from "chai";
 
 
-describe("Helper functionality", async function () {
+describe("Plugin tests", async function () {
     describe("successful-compilation artifact", async function () {
         useEnvironment("successful-compilation");
 
         it("Should load artifacts", async function () {
-            // console.log(`Artifacts: ${JSON.stringify(this.env.artifacts)}`);
             const artifactExists = await this.env.artifacts.artifactExists("Greeter");
             assert(artifactExists, "Greeter artifact doesn't exist");
+
+            const artifact = await this.env.artifacts.readArtifact("Greeter");
+            assert(artifact._format == "hh-zksolc-artifact-1", "Incorrect artifact build");
+
+            // Check that we can load an additional key (it turns that we can which is great).
+            assert((artifact as any)["_additionalKey"] == "some_value", "Additional key not loaded!");
         })
     })
 })
