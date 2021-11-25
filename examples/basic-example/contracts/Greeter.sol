112327ac10b2ff26dd6172e7d110b4abd718aa2b
Igor Aleksanov <popzxc@yandex.ru>
2021-11-25 11:22:35 +0300

    Complete the deploy script

--- examples/basic-example/contracts/Greeter.sol
+++ examples/basic-example/contracts/Greeter.sol
@@ -4,12 +4,9 @@ pragma solidity ^0.8.0;
 pragma abicoder v2;
 
 contract Greeter {
-
     string greeting;
-    string bad;
     constructor(string memory _greeting) {
         greeting = _greeting;
-        bad = "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad";
     }
 
     function greet() public view returns (string memory) {
--- examples/basic-example/deploy/001_deploy.ts
+++ examples/basic-example/deploy/001_deploy.ts
@@ -5,6 +5,8 @@ import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
 // An example of a deploy script which will deploy and call a simple contract.
 export default async function(hre: HardhatRuntimeEnvironment) {
+    console.log(`Running deploy script for the Greeter contract`);
+
     // Initialize an Ethereum wallet.
     const testMnemonic = "stuff slice staff easily soup parent arm payment cotton trade scatter struggle";
     const ethWallet = ethers.Wallet.fromMnemonic(testMnemonic, "m/44'/60'/0'/0/0");
@@ -16,15 +18,31 @@ export default async function(hre: HardhatRuntimeEnvironment) {
     // Create a zkSync wallet using the Ethereum wallet created above.
     const zkWallet = new zk.Wallet(ethWallet.privateKey, zkWeb3Provider, ethWeb3Provider);
 
+    // Deposit some funds to L2 in order to be able to perform deposits.
+    // const depositAmount = ethers.utils.parseEther("0.001"); // TODO: Why parseEther doesn't work?
+    const depositHandle = await zkWallet.deposit({
+        to: zkWallet.address,
+        token: zk.utils.ETH_ADDRESS,
+        amount: "1000000000000000000", // TODO: Why parseEther doesn't work?
+    });
+    await depositHandle.wait();
+
     // Create deployer object and load desired artifact.
     const deployer = new Deployer(hre, zkWallet);
     const artifact = await deployer.loadArtifact("Greeter");
 
     // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
-    const greeterContract = await deployer.deploy(artifact, ["Hi there!"]);
+    const greeting = "Hi there!";
+    const greeterContract = await deployer.deploy(artifact, [greeting]);
 
-    const contractAddress = greeterContract.contractAddress;
+    const contractAddress = greeterContract.address;
     console.log(`${artifact.contractName} was deployed to ${contractAddress}!`);
 
-    // TODO: add an example of calling the contract.
+    // Call the deployed contract.
+    const greetingFromContract = await greeterContract.greet();
+    if (greetingFromContract == greeting) {
+        console.log(`Contract greets us!`);
+    } else {
+        console.error(`Contract said something unexpected: ${greetingFromContract}`);
+    }
 }
