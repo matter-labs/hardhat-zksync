2b36969629b21806584b00861761758f3c7f8cac
FilipTxFusion <filip@txfusion.io>
2022-11-17 17:42:04 +0100

    Updated readme, removed deploy dependency

--- packages/hardhat-zksync-verify/test/common.config.ts
+++ packages/hardhat-zksync-verify/test/common.config.ts
@@ -1,19 +1,7 @@
 import '@nomiclabs/hardhat-etherscan';
 import '@matterlabs/hardhat-zksync-solc';
-import '@matterlabs/hardhat-zksync-deploy';
 import '../src/index';
 import { HardhatUserConfig } from 'hardhat/config';
-
-const zkSyncDeploy =
-    process.env.NODE_ENV == 'test'
-        ? {
-              zkSyncNetwork: 'http://localhost:3050',
-              ethNetwork: 'http://localhost:8545',
-          }
-        : {
-              zkSyncNetwork: 'https://zksync2-testnet.zksync.dev',
-              ethNetwork: 'goerli',
-          };
 
 const config: HardhatUserConfig = {
     zksolc: {
@@ -34,7 +22,6 @@ const config: HardhatUserConfig = {
             verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
         },
     },
-    zkSyncDeploy,
     solidity: {
         version: '0.8.16',
     },
