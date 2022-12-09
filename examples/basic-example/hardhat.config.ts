869ed633a437a2a9a33b7074f24ea45098c063ca
FilipTxFusion <filip@txfusion.io>
2022-12-09 17:31:08 +0100

    Updated dependecies, reverted basic examples config

--- examples/basic-example/hardhat.config.ts
+++ examples/basic-example/hardhat.config.ts
@@ -1,13 +1,11 @@
 import '@matterlabs/hardhat-zksync-deploy';
 import '@matterlabs/hardhat-zksync-solc';
-import '@matterlabs/hardhat-zksync-verify';
-import '@nomiclabs/hardhat-etherscan';
 
 import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
     zksolc: {
-        version: '1.2.0',
+        version: '1.2.1',
         compilerSource: 'binary',
     },
     networks: {
@@ -18,7 +16,7 @@ const config: HardhatUserConfig = {
     // Docker image only works for solidity ^0.8.0.
     // For earlier versions you need to use binary releases of zksolc.
     solidity: {
-        version: '0.8.16',
+        version: '0.8.17',
     },
 };
 
