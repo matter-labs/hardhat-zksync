63844f7f76c4dd1b430b8a7cfa262132e8896942
FilipTxFusion <filip@txfusion.io>
2022-12-26 13:53:29 +0100

    removed vyper cache changes

--- packages/hardhat-zksync-vyper/src/constants.ts
+++ packages/hardhat-zksync-vyper/src/constants.ts
@@ -1,6 +1,1 @@
 export const SOLIDITY_EXTENSION = '.sol';
-export const DEFAULT_VYPER_VERSION = '0.3.1';
-export const ARTIFACT_FORMAT_VERSION = 'hh-vyper-artifact-1';
-export const DEBUG_NAMESPACE = 'hardhat:plugin:vyper';
-export const CACHE_FORMAT_VERSION = 'hh-vy-cache-1';
-export const VYPER_FILES_CACHE_FILENAME = 'vyper-files-cache.json';
