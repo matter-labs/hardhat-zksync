6f697384db42459630f65ceae8f5b039d20d04fd
FilipTxFusion <filip@txfusion.io>
2023-05-23 14:36:33 +0200

    Update proxy json import

--- packages/hardhat-zksync-upgradable/src/constants.ts
+++ packages/hardhat-zksync-upgradable/src/constants.ts
@@ -1,9 +1,9 @@
-export const ITUP_JSON = '/ITransparentUpgradeableProxy.json';
-export const TUP_JSON = '/TransparentUpgradeableProxy.json';
-export const BEACON_PROXY_JSON = '/BeaconProxy.json';
-export const UPGRADABLE_BEACON_JSON = '/UpgradeableBeacon.json';
-export const PROXY_ADMIN_JSON = '/ProxyAdmin.json';
-export const ERC1967_PROXY_JSON = '/ERC1967Proxy.json';
+export const ITUP_JSON = 'ITransparentUpgradeableProxy.json';
+export const TUP_JSON = 'TransparentUpgradeableProxy.json';
+export const BEACON_PROXY_JSON = 'BeaconProxy.json';
+export const UPGRADABLE_BEACON_JSON = 'UpgradeableBeacon.json';
+export const PROXY_ADMIN_JSON = 'ProxyAdmin.json';
+export const ERC1967_PROXY_JSON = 'ERC1967Proxy.json';
 export const LOCAL_SETUP_ZKSYNC_NETWORK = 'http://localhost:3050';
 export const FORMAT_TYPE_MINIMAL = 'minimal';
 export const MANIFEST_DEFAULT_DIR = '.upgradable';
