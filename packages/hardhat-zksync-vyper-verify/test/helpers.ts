241c22979a15f29c49bddc5eeafd6565c5b59e7a
FilipTxFusion <filip@txfusion.io>
2023-07-25 16:50:29 +0200

    Add test compilation

--- packages/hardhat-zksync-vyper-verify/test/helpers.ts
+++ packages/hardhat-zksync-vyper-verify/test/helpers.ts
@@ -3,6 +3,7 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { TASK_CLEAN } from 'hardhat/builtin-tasks/task-names';
 import '../src/type-extensions';
 import path from 'path';
+import { TASK_COMPILE_VYPER } from '../src/constants';
 declare module 'mocha' {
     interface Context {
         env: HardhatRuntimeEnvironment;
@@ -10,10 +11,12 @@ declare module 'mocha' {
 }
 
 export function useEnvironment(fixtureProjectName: string, networkName = 'hardhat') {
-    beforeEach('Loading hardhat environment', function () {
+    beforeEach('Loading hardhat environment', async function () {
         process.chdir(path.join(__dirname, 'fixture-projects', fixtureProjectName));
         process.env.HARDHAT_NETWORK = networkName;
         this.env = require('hardhat');
+        await this.env.run(TASK_COMPILE_VYPER, { quiet: false});
+
     });
 
     afterEach('Resetting hardhat', function () {
