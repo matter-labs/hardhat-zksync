8ced4be8607adbc2a77766cd71bc226c4b134a08
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2023-10-30 18:20:01 +0100

    removed zksync-web3 and added zksync2-js to examples and packages

--- examples/node-example/test/tests.ts
+++ examples/node-example/test/tests.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { Contract, Wallet, Provider } from "zksync-web3";
+import { Contract, Wallet, Provider } from "zksync2-js";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 import { ZkSyncArtifact } from '@matterlabs/hardhat-zksync-deploy/src/types';
 import "@matterlabs/hardhat-zksync-node/dist/type-extensions";
