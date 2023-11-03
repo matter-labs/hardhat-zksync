55dbb294d4511f9b210b869e5b27e7caec6d3069
Marko Arambasic <makiarambasic@gmail.com>
2023-11-03 16:50:00 +0100

    fix: removed unnecessery deps and zk.Contract

--- examples/zksync2js-example/deploy/001_deploy.ts
+++ examples/zksync2js-example/deploy/001_deploy.ts
@@ -5,8 +5,8 @@ import { Contract } from 'zksync2-js';
 export default async function (hre: HardhatRuntimeEnvironment) {
     console.info(chalk.yellow(`Running deploy`));
     const greeterFactory = await hre.zksync2js.getContractFactory("Greeter");
-    const greeter = await greeterFactory.deploy("Hello, world!") as Contract;
-
+    const greeter = await greeterFactory.deploy("Hello, world!");
+    
     console.info(chalk.green(`Greeter deployed to: ${await greeter.getAddress()}`));
     console.info(chalk.green(`Greeter greeting set to: ${await greeter.greet()}`));
     const tx = await greeter.setGreeting("Hello, world again!");
