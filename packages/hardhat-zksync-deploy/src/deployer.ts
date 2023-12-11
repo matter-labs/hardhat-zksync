0553019c041295d9adc3df0d50447fc6d02be33d
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2023-12-11 10:09:52 +0100

    fix(hardhat-zksync-deploy): fixed edge case when url for ethNetwork is provided

--- packages/hardhat-zksync-deploy/src/deployer.ts
+++ packages/hardhat-zksync-deploy/src/deployer.ts
@@ -5,7 +5,7 @@ import * as ethers from 'ethers';
 import { ZkSyncArtifact } from './types';
 import { ZkSyncDeployPluginError } from './errors';
 import { ETH_DEFAULT_NETWORK_RPC_URL } from './constants';
-import { isHttpNetworkConfig } from './utils';
+import { isHttpNetworkConfig, isValidEthNetworkURL } from './utils';
 
 const ZKSOLC_ARTIFACT_FORMAT_VERSION = 'hh-zksolc-artifact-1';
 const ZKVYPER_ARTIFACT_FORMAT_VERSION = 'hh-zkvyper-artifact-1';
@@ -86,10 +86,16 @@ export class Deployer {
                     ? new ethers.JsonRpcProvider((networks[ethNetwork] as HttpNetworkConfig).url)
                     : ethers.getDefaultProvider(ethNetwork);
         } else {
-            ethWeb3Provider =
-                ethNetwork === 'localhost' || ethNetwork === ''
-                    ? this._createDefaultEthProvider()
-                    : new ethers.JsonRpcProvider((networks[ethNetwork] as HttpNetworkConfig).url);
+            if (ethNetwork === 'localhost' || ethNetwork === '') {
+                ethWeb3Provider = this._createDefaultEthProvider();
+            } else if (isValidEthNetworkURL(ethNetwork)) {
+                ethWeb3Provider = new ethers.JsonRpcProvider(ethNetwork);
+            } else {
+                ethWeb3Provider =
+                    ethNetwork in networks && isHttpNetworkConfig(networks[ethNetwork])
+                        ? new ethers.JsonRpcProvider((networks[ethNetwork] as HttpNetworkConfig).url)
+                        : ethers.getDefaultProvider(ethNetwork);
+            }
         }
 
         zkWeb3Provider = new zk.Provider((network.config as HttpNetworkConfig).url);
