279e1c04297a9c92050a877478b8070d9cc09068
Marko Arambasic <makiarambasic@gmail.com>
2023-12-19 23:34:10 +0100

    fix(hardhat-zksync-deploy): revert to previous compiler version

--- examples/basic-example/hardhat.config.ts
+++ examples/basic-example/hardhat.config.ts
@@ -20,37 +20,8 @@ const config: HardhatUserConfig = {
     },
     // Docker image only works for solidity ^0.8.0.
     // For earlier versions you need to use binary releases of zksolc.
-    // solidity: {
-    //     version: "0.8.17",
-    //     eraVersion: "0.8.17",
-    //     settings: {
-    //         optimizer: {
-    //             enabled: true,
-    //         },
-
-    //     },
-    // }
     solidity: {
-        overrides: {
-            "contracts/001_deploy/Greeter.sol": {
-                version: "0.8.16",
-            },
-            "contracts/002_factory/Foo.sol": {
-                version: "0.8.16",
-                eraVersion: "latest"
-            }
-        },
-        compilers: [
-            {
-                version: "0.8.17",
-                eraVersion: "latest",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                    },
-                },
-            }
-        ]
+        version: '0.8.17'
     }
 };
 
