37dbdec1901947ab027941266afc6969b37014b0
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2024-02-09 12:43:25 +0100

    chore: remove libraries from config

--- examples/noninline-libraries-example/hardhat.config.ts
+++ examples/noninline-libraries-example/hardhat.config.ts
@@ -6,17 +6,6 @@ import { HardhatUserConfig } from 'hardhat/config';
 const config: HardhatUserConfig = {
     zksolc: {
         settings: {
-            libraries: {
-                  "contracts/ChildChildLib.sol": {
-                    "ChildChildLib": "0x41a46cc4f25F8d2C50527cF2cDD4b8c124DEe40D"
-                  },
-                  "contracts/ChildLib.sol": {
-                    "ChildLib": "0x3fbFf2EF1F865A141eD3d393b16bFc9B4E9418F3"
-                  },
-                  "contracts/MathLib.sol": {
-                    "MathLib": "0xAaF1842C3660C163D2075feCbDb31c26e0e25e81"
-                  }
-                }
         }
     },
     defaultNetwork: 'zkSyncNetwork',
