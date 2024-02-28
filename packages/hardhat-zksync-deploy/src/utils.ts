909c05ee4af0c125db9c40ef9939cb004d056994
Marko Arambasic <makiarambasic@gmail.com>
2024-02-28 16:40:15 +0100

    fix: support for non-inline libraries 'export default' expressions and zksolc is not reqired for config update

--- packages/hardhat-zksync-deploy/src/utils.ts
+++ packages/hardhat-zksync-deploy/src/utils.ts
@@ -10,7 +10,7 @@ import {
 import fs from 'fs';
 import { Wallet } from 'zksync-ethers';
 import { ContractFullQualifiedName, ContractInfo, MissingLibrary } from './types';
-import { MorphTsBuilder } from './morph-ts-builder';
+import { MorphBuilderInitialDefaultAssignment, MorphTsBuilder } from './morph-ts-builder';
 import { ZkSyncDeployPluginError } from './errors';
 import { LOCAL_CHAIN_IDS } from './constants';
 import { richWallets } from './rich-wallets';
@@ -27,11 +27,12 @@ export function updateHardhatConfigFile(
     try {
         new MorphTsBuilder(externalConfigObjectPath ?? hre.config.paths.configFile)
             .intialStep([
+                { initialModule: 'module.exports' },
+                {} as MorphBuilderInitialDefaultAssignment,
                 { initialVariableType: 'HardhatUserConfig' },
                 { initialVariable: exportedConfigObject },
-                { initialModule: 'module.exports' },
             ])
-            .nextStep({ propertyName: 'zksolc', isRequired: true })
+            .nextStep({ propertyName: 'zksolc' })
             .nextStep({ propertyName: 'settings' })
             .replaceStep({ propertyName: 'libraries', replaceObject: hre.config.zksolc.settings.libraries })
             .save();
