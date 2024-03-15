4b3cc5016c8cbb833fb81f6ae05be14be351c5b3
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2024-03-15 13:22:34 +0100

    chore: run test in dockerized node

--- e2e/fixture-projects/compatability-check/test.sh
+++ e2e/fixture-projects/compatability-check/test.sh
@@ -8,26 +8,16 @@ echo "Running compatability-check"
 
 run_test_and_handle_failure "yarn hardhat compile" 0
 
-kill_process_on_port 8011
-
-yarn hardhat node-zksync &
-
-while ! curl -s -X POST -d '{"jsonrpc":"2.0","method":"net_version","id":1}' -H 'Content-Type: application/json' 0.0.0.0:8011; do
-    echo "Waiting for node-zksync to start..."
-    sleep 1
-done
-
-yarn hardhat deploy-zksync:libraries --network inMemoryNode
+yarn hardhat deploy-zksync:libraries --network dockerizedNode
 
 yarn hardhat compile
 
-yarn hardhat deploy-zksync --network inMemoryNode
+yarn hardhat deploy-zksync --network dockerizedNode
 
 assert_directory_exists "artifacts-zk"
 assert_directory_exists "cache-zk"
 
-
-
+yarn hardhat run scripts/deploy-factory-uups.ts --network inMemoryNode
 
 
 
