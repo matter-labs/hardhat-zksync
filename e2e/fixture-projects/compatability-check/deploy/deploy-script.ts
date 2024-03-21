ddf1a82f34dde41d496eaaf3a70937673b3f414d
Marko Arambasic <131957563+kiriyaga-txfusion@users.noreply.github.com>
2024-03-21 11:47:32 +0100

    Revert "chore: add e2e tests (#869)" (#937)
    This reverts commit 5c05619e10f598e85a739c03dec9064c14928a5e.

--- e2e/fixture-projects/compatability-check/deploy/deploy-script.ts
+++
@@ -1,7 +0,0 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-
-export default async function (hre: HardhatRuntimeEnvironment) {
-    console.info(`Running deploy script for the Test contract`);
-    await hre.deployer.deploy("TestCoin", []);
-    console.info(`Test coin was deployed!`);
-}
