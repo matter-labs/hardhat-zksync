930935959a5517d2d7f5f6773bf3d8d99d8f77fd
Marko Arambasic <makiarambasic@gmail.com>
2024-04-01 00:08:16 +0200

    fix: update regex to support new lines

--- packages/hardhat-zksync-verify/src/constants.ts
+++ packages/hardhat-zksync-verify/src/constants.ts
@@ -109,17 +109,17 @@ This means that unrelated contracts may be displayed on the zksync block explore
 export const COMPILERS_CONFLICT_ZKVM_SOLC = (version: string) =>
     `Solidity compiler versions in your Hardhat config file are in conflict for version ${version}. Please specify version of compiler only with zkVm support(eraVersion) or without it`;
 
-export const COMPILATION_ERRORS = [
-    {
-        error: ' CompilationError',
-        pattern: /^.*Compilation error.*$/,
-    },
-    {
-        error: 'MissingSource',
-        pattern: /^.*There is no .* source file$/,
-    },
-    {
-        error: 'MissingContract',
-        pattern: /^.*Contract with .* name is missing in sources$/,
-    },
-];
+    export const COMPILATION_ERRORS = [
+      {
+          error: ' CompilationError',
+          pattern: /^Backend verification error: Compilation error.*$/s,
+      },
+      {
+          error: 'MissingSource',
+          pattern: /^Backend verification error: There is no .* source file$/,
+      },
+      {
+          error: 'MissingContract',
+          pattern: /^Backend verification error: Contract with .* name is missing in sources$/,
+      },
+  ];
