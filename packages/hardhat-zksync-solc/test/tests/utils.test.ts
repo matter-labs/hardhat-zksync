bc31f1d1377d39f7d245108f8cf480bd216d7785
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2024-04-24 19:09:52 +0200

    fix: architecture mock

--- packages/hardhat-zksync-solc/test/tests/utils.test.ts
+++ packages/hardhat-zksync-solc/test/tests/utils.test.ts
@@ -272,9 +272,11 @@ describe('Utils', () => {
 
 describe('getZksolcUrl', () => {
     const platformStub = sinon.stub(process, 'platform');
-
+    
     it('should return the release URL when isRelease is true', () => {
         platformStub.value('linux'); // Mock the process.platform property
+        const archStub = sinon.stub(process, 'arch');
+        archStub.value('x64');
         const repo = 'example/repo';
         const version = '1.0.0';
         let expectedUrl = 'example/repo/releases/download/v1.0.0/zksolc-linux-amd64-musl-v1.0.0';
@@ -293,6 +295,8 @@ describe('getZksolcUrl', () => {
 
     it('should return the raw URL when isRelease is false', () => {
         platformStub.value('linux'); // Mock the process.platform property
+        const archStub = sinon.stub(process, 'arch');
+        archStub.value('x64'); 
         const repo = 'example/repo';
         const version = '1.0.0';
         let expectedUrl = 'example/repo/raw/main/linux-amd64/zksolc-linux-amd64-musl-v1.0.0';
