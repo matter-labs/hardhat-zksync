059734297063ae7f2e048b442d4c75d5753041c7
nikola-bozin-txfusion <nikolabozin@txfusion.io>
2024-04-25 14:29:41 +0200

    chore: deprecate hardhat-zksync-chai-matchers

--- e2e/fixture-projects/node/test.sh
+++ e2e/fixture-projects/node/test.sh
@@ -18,7 +18,7 @@ sleep 3
 
 LOG_FILE="./era_test_node.log"
 
-if grep -q "Node is ready at 127.0.0.1:8011" "$LOG_FILE"; then
+if grep -q "Node is ready at 0.0.0.0:8011" "$LOG_FILE"; then
     echo "zkSync node started successfully."
 else
     echo "Failed to start zkSync node. Exiting with code 1."
