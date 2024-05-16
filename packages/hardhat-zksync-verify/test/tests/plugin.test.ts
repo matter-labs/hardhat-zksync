186268eda05590aad5db421c3c2ff348bd8bd62c
Marko Arambasic <makiarambasic@gmail.com>
2024-05-16 15:48:38 +0200

    fix: change ci solidity version to 0.6.12

--- packages/hardhat-zksync-verify/test/tests/plugin.test.ts
+++ packages/hardhat-zksync-verify/test/tests/plugin.test.ts
@@ -332,7 +332,7 @@ Instead, this name was received: ${contractFQN}`);
             };
 
             const solidityStandardJsonInput3 = getSolidityStandardJsonInput(hre2 as any, resolvedFiles, input);
-            expect(solidityStandardJsonInput3.settings.enableEraVMExtensions).to.equal(false);
+            expect(solidityStandardJsonInput3.settings.isSystem).to.equal(false);
             expect(solidityStandardJsonInput3.settings.forceEvmla).to.equal(true);
 
             const hre3 = {
