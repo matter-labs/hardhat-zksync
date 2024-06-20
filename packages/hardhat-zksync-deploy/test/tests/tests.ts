ab3cfbac4804560787f58a1c85f7c5b8510788fe
Marko Arambasic <makiarambasic@gmail.com>
2024-06-20 13:57:06 +0200

    chore: skip test on the wrong network

--- packages/hardhat-zksync-deploy/test/tests/tests.ts
+++ packages/hardhat-zksync-deploy/test/tests/tests.ts
@@ -192,7 +192,7 @@ describe('Plugin tests', async function () {
             }
         });
 
-        it('Should estimate deploy fee', async function () {
+        it.skip('Should estimate deploy fee', async function () {
             const zkWallet = new Wallet(WALLET_PRIVATE_KEY);
             const deployer = new Deployer(this.env, zkWallet);
             await this.env.run('compile');
