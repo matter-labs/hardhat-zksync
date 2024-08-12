d1588ec2beb18acaf287ec315f05d1ab93108f65
Marko Arambasic <makiarambasic@gmail.com>
2024-08-12 12:46:52 +0200

    fix: replace example networks

--- examples/zksync-ethers-example/hardhat.config.ts
+++ examples/zksync-ethers-example/hardhat.config.ts
@@ -14,22 +14,13 @@ const config: HardhatUserConfig = {
             },
         }
     },
-    defaultNetwork: 'hardhat',
+    defaultNetwork: 'zkSyncLocal',
     networks: {
-        hardhat: {
-            accounts: [
-                {privateKey: '0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110', balance: '1000000000000000000000000'}
-            ]
-        },  
         zkSyncLocal: {
             zksync: true,
             url: "http://0.0.0.0:3050",
             ethNetwork: 'http://0.0.0.0:8545',
         },
-        eth: {
-            url: "http://0.0.0.0:8545",
-            accounts: ['0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110']
-        }
     },
     // Docker image only works for solidity ^0.8.0.
     // For earlier versions you need to use binary releases of zksolc.
