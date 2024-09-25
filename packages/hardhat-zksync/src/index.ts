a7beaa9b9395c41d04458f1e6713b2988a4d8742
Marko Arambasic <makiarambasic@gmail.com>
2024-09-25 12:34:49 +0200

    feat: remove zksync contracts dependecy, update hardhat-zksync-solc version and add hardhat-zksync-ethers as dependency

--- packages/hardhat-zksync/src/index.ts
+++ packages/hardhat-zksync/src/index.ts
@@ -5,6 +5,7 @@ import '@matterlabs/hardhat-zksync-solc';
 import '@matterlabs/hardhat-zksync-deploy';
 import '@matterlabs/hardhat-zksync-verify';
 import '@matterlabs/hardhat-zksync-upgradable';
+import '@matterlabs/hardhat-zksync-ethers';
 
 import {
     TASK_DEPLOY_ZKSYNC_BEACON,
