f4358f0f68425f5e0fcbc249ecd5f9d7336d10ba
Marko Arambasic <makiarambasic@gmail.com>
2024-11-29 13:55:54 +0100

    chore: test e2e test for node-zksync task with port 8012

--- e2e/fixture-projects/node/test.sh
+++ e2e/fixture-projects/node/test.sh
@@ -12,7 +12,7 @@ sleep 10
 
 LOG_FILE="./era_test_node.log"
 
-if grep -q "Node is ready at 127.0.0.1:8012" "$LOG_FILE"; then
+if grep -q "Listening on 127.0.0.1:8012" "$LOG_FILE"; then
     echo "ZKsync node started successfully."
 else
     echo "Failed to start ZKsync node. Exiting with code 1."
